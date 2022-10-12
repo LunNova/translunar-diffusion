5101517c041f18c630c791d664271d00e4960ddc
Luna Nova <git@lunnova.dev>
2022-10-11 19:53:27 -0700

    callbacks.ImageLogger: simplify and add separate val steps checks

--- callbacks/__init__.py
+++ callbacks/__init__.py
@@ -64,35 +64,27 @@ class ImageLogger(Callback):
         self,
         batch_frequency,
         max_images,
+        val_batch_frequency=1,
         clamp=True,
-        increase_log_steps=True,
         rescale=True,
         disabled=False,
-        log_on_batch_idx=False,
-        log_first_step=False,
         log_images_kwargs=None,
-        check_custom_step=False,
+        log_images_to_loggers=False,
     ):
         super().__init__()
         self.rescale = rescale
         self.batch_freq = batch_frequency
+        self.val_batch_freq = val_batch_frequency
         self.max_images = max_images
-        self.log_steps = [
-            2**n for n in range(int(np.log2(self.batch_freq)) + 1)
-        ]
-        if not increase_log_steps:
-            self.log_steps = [self.batch_freq]
         self.clamp = clamp
-        self.disabled = disabled
-        self.log_on_batch_idx = log_on_batch_idx
-        self.log_images_kwargs = log_images_kwargs if log_images_kwargs else {}
-        self.log_first_step = log_first_step
-        self.check_custom_step = check_custom_step
+        self.disabled = disabled or max_images <= 0
+        self.log_images_kwargs = log_images_kwargs or {}
         self.custom_step = 0
+        self.custom_val_step = 0
+        self.log_images_to_loggers = log_images_to_loggers
 
-    @rank_zero_only
     def log_local(
-        self, save_dir, split, images, global_step, current_epoch, batch_idx
+        self, save_dir, split, images, global_step, current_epoch,
     ):
         root = os.path.join(save_dir, 'images', split)
         for k in images:
@@ -102,35 +94,25 @@ class ImageLogger(Callback):
             grid = grid.transpose(0, 1).transpose(1, 2).squeeze(-1)
             grid = grid.numpy()
             grid = (grid * 255).astype(np.uint8)
-            filename = '{}_gs-{:06}_e-{:06}_b-{:06}.png'.format(
-                k, global_step, current_epoch, batch_idx
+            filename = '{}_r-{}_gs-{:06}_e-{:06}.png'.format(
+                k, rank_zero_only.rank, global_step, current_epoch,
             )
             path = os.path.join(root, filename)
             os.makedirs(os.path.split(path)[0], exist_ok=True)
             Image.fromarray(grid).save(path)
 
-    def log_img(self, pl_module, batch, batch_idx, split='train'):
-        check_idx = (
-            self.custom_step
-            if self.check_custom_step
-            else (
-                batch_idx if self.log_on_batch_idx else pl_module.global_step
-            )
-        )
-        if (
-            self.check_frequency(check_idx)
-            and hasattr(pl_module, 'log_images')
-            and callable(pl_module.log_images)
-            and self.max_images > 0
-        ):
-            is_train = pl_module.training
-            if is_train:
-                pl_module.eval()
+    def log_img(self, pl_module, batch, split='train'):
+        if not hasattr(pl_module, 'log_images'):
+            return
 
-            with torch.no_grad():
-                images = pl_module.log_images(
-                    batch, split=split, **self.log_images_kwargs
-                )
+        is_train = pl_module.training
+        if is_train:
+            pl_module.eval()
+
+        with torch.no_grad():
+            images = pl_module.log_images(
+                batch, split=split, **self.log_images_kwargs
+            )
 
             for k in images:
                 N = min(images[k].shape[0], self.max_images)
@@ -142,7 +124,7 @@ class ImageLogger(Callback):
 
             save_dir = None
             for logger in pl_module.loggers:
-                if hasattr(logger, 'experiment') and hasattr(
+                if self.log_images_to_loggers and hasattr(logger, 'experiment') and hasattr(
                     logger.experiment, 'add_image'
                 ):
                     for k in images:
@@ -169,39 +151,26 @@ class ImageLogger(Callback):
                     images,
                     pl_module.global_step,
                     pl_module.current_epoch,
-                    batch_idx,
                 )
 
-            if is_train:
-                pl_module.train()
-
-    def check_frequency(self, check_idx):
-        if (
-            (check_idx % self.batch_freq) == 0 or (check_idx in self.log_steps)
-        ) and (check_idx > 0 or self.log_first_step):
-            try:
-                self.log_steps.pop(0)
-            except IndexError as e:
-                print(e)
-                pass
-            return True
-        return False
+        if is_train:
+            pl_module.train()
 
     def on_train_batch_end(
         self, trainer, pl_module, outputs, batch, batch_idx
     ):
-        if not self.disabled and (
-            pl_module.global_step > 0 or self.log_first_step
-        ):
+        if not self.disabled:
             self.custom_step += 1
-            self.log_img(pl_module, batch, batch_idx, split='train')
+            if self.custom_step % self.batch_freq == 0:
+                self.log_img(pl_module, batch, split='train')
 
     def on_validation_batch_end(
         self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx
     ):
-        if not self.disabled and pl_module.global_step > 0:
-            self.custom_step += 1
-            self.log_img(pl_module, batch, batch_idx, split='val')
+        if not self.disabled:
+            self.custom_val_step += 1
+            if self.custom_val_step % self.val_batch_freq == 0:
+                self.log_img(pl_module, batch, split='val')
         if hasattr(pl_module, 'calibrate_grad_norm'):
             if (
                 pl_module.calibrate_grad_norm and batch_idx % 25 == 0
