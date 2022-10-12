cf63c8581abeb8b959cbcf12386ee77c93284861
Luna Nova <git@lunnova.dev>
2022-10-11 19:59:14 -0700

    train-txt2img.sh: handle split of training configs

--- scripts/train-txt2img.sh
+++ scripts/train-txt2img.sh
@@ -9,8 +9,8 @@ export ROCR_VISIBLE_DEVICES=1
 
 
 exec python lun/main.py \
-  model.base_learning_rate=8e-6 lightning.trainer.accumulate_grad_batches=1 \
- --base ./lun/configs/training-txt2img.yaml \
+  model.base_learning_rate=2.5e-6 lightning.trainer.accumulate_grad_batches=1 data.params.batch_size=2 \
+ --base ./lun/configs/training-lightning.yaml ./lun/configs/training-txt2img.yaml ./lun/local/training-derpi-sfw.yaml \
  --train=True \
  --accelerator gpu --devices 0, \
  --resume logs/pony-sfw-finetune/2022-09-26T21-18-20_v1.4/ \
