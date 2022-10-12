297406dd034ffeff72357fa33adf7f0e2b0c4f12
Luna Nova <git@lunnova.dev>
2022-10-11 19:53:52 -0700

    training-encoder: update for name changes

--- scripts/train-autoencoder.sh
+++ scripts/train-autoencoder.sh
@@ -6,8 +6,8 @@ export ROCR_VISIBLE_DEVICES=1
 # --actual_resume models/sd-v1.4-kl.ckpt -n 0 \
 
 exec python lun/main.py \
-  model.base_learning_rate=1e-4 \
- --base ./lun/configs/training-encoder.yaml \
+  model.base_learning_rate=5e-6 \
+ --base ./lun/configs/training-lightning.yaml ./lun/configs/training-encoder.yaml \
  --train \
  --accelerator gpu --devices 0, \
  --resume logs/encoder-kl-f8-derpibooru/2022-09-27T11-39-13_0/ \
