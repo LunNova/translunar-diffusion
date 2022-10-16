ef6b909e3bf459c77657b93ea1a334bc39ed5c7a
Luna Nova <git@lunnova.dev>
2022-10-15 21:36:46 -0700

    docker/start.sh: fix cuda device handling and some paths

--- docker/start.sh
+++ docker/start.sh
@@ -5,26 +5,38 @@
 # Defaults to cuda
 # If force set, rebuilds even if tag already exists
 
+# FIXME: docker support? This is currently podman only
+
 set -xeuo pipefail
 
 type=${1:-cuda}
 
-tag=lun-diffusion-$type:latest
-container=lun-diffusion-$type-container
+tag=translunar-diffusion-$type:latest
+container=translunar-diffusion-$type-container
 
-cp ../../scripts/preload_models.py preload.py
+echo "Using tag $tag"
+
+if [ "$type" = "rocm" ]; then
+  devices="--device /dev/kfd --device /dev/dri"
+else
+  # FIXME: pass through more than just 0
+  devices="--device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm"
+fi
+
+cp ../diffusion/scripts/preload_models.py preload.py
 
 if ! podman inspect --type=image $tag || [[ "z${2:-}" = "zforce" ]]; then
   podman build -f $type.Dockerfile --format docker -t $tag .
 fi
 podman rm $container || true
-podman run --name $container --rm -it -v $(realpath ../../):/content/ \
+podman run --name $container --rm -it -v $(realpath ../):/content/ \
   -v /mnt/scratch/derpibooru/:/inputs/derpibooru/:ro \
  --userns=keep-id --ipc=host \
   -u 1000:1000 \
-  --device /dev/kfd --device /dev/dri --security-opt seccomp=unconfined --group-add video \
+  $devices \
+  --security-opt seccomp=unconfined --group-add video \
   --network host \
   --log-driver none \
   $tag \
-  bash -c ". /usr/local/bin/_activate_current_env.sh && id && cd /content && ls -l . && pip install --user -e . && exec bash" || true
+  bash -c ". /usr/local/bin/_activate_current_env.sh && id && cd /content && ls -l . && exec bash" || true
 podman rm $container || true
